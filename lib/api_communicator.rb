d14b61a91a30b8a00679c3bf2bbbb6255f2e0124
patti-c <patricia.carter@flatironschool.com>
2018-08-28 15:37:57 -0400

    message

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,33 +3,52 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
+
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
+  api_urls = {}
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  response_hash["results"].each do |character_hash|
+    if character_hash["name"].downcase == character
+      api_urls = character_hash["films"]
+    end
+  end
+
+  film_array = []
+
+  api_urls.each do |film_url|
+    film_string = RestClient.get(film_url)
+    film_array << JSON.parse(film_string)
+  end
+
+  film_array
 end
+
+# # puts "1) *******"
+#  puts get_character_movies_from_api('luke skywalker')
+# puts "2) *******"
+# puts get_character_movies_from_api('c-3po')
+# puts "3) *******"
 
 def print_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    # binding.pry
+    puts film["title"]
+  end
 end
 
 def show_character_movies(character)
+  # binding.pry
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
 end
+#
+# show_character_movies('han solo')
+# show_character_movies('c-3Po')
+#show_character_movies('c-3po')
+# show_character_movies('Princess Leia')
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,11 @@
 def welcome
   # puts out a welcome message here!
+  puts 'Welcome Jedi. May the force be with you.'
+  puts 'You have the high ground.'
 end
 
 def get_character_from_user
   puts "please enter a character"
+  user_input = gets.chomp.downcase
   # use gets to capture the user's input. This method should return that input, downcased.
 end
